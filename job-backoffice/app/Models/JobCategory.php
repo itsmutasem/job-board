e48b8fffd4cef9c3c08a36f053331e711068c7f9
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-14 19:04:02 +0300

    feat(job category model): set uuid, fillable, and soft delete "deleted_at"

--- job-backoffice/app/Models/JobCategory.php
+++ job-backoffice/app/Models/JobCategory.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobCategory extends Model
 {
-    //
+    use HasFactory, HasUuids, SoftDeletes;
+    protected $table = 'job_categories';
+    protected $keyType = 'sting';
+    public $incrementing = false;
+
+    protected $fillable = [
+      'name',
+    ];
+
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'deleted_at' => 'datetime',
+        ];
+    }
 }
