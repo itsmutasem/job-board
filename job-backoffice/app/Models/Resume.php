0b7bf2e7a07cbca17dc96bb66cc9acfb27eead5d
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-14 20:06:38 +0300

    feat(resume model): set uuid, fillable, soft delete

--- job-backoffice/app/Models/Resume.php
+++ job-backoffice/app/Models/Resume.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Resume extends Model
 {
-    //
+    Use HasFactory, HasUuids, SoftDeletes;
+    protected $table = 'resumes';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'filename',
+        'fileUri',
+        'contactDetails',
+        'summary',
+        'skills',
+        'experience',
+        'education',
+        'userId',
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    protected function casts()
+    {
+        return [
+            'deleted_at' => 'datetime'
+        ];
+    }
 }
