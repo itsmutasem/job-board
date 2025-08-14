4071926fd40f183c61d8321b44b583b6853ae0ff
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-14 19:09:35 +0300

    feat(company model): set uuid, fillable, and soft delete

--- job-backoffice/app/Models/Company.php
+++ job-backoffice/app/Models/Company.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    //
+    use HasFactory, HasUuids, SoftDeletes;
+    protected $table = 'companies';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'industry',
+        'website',
+        'ownerId',
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'deleted_at' => 'datetime'
+        ];
+    }
 }
