f83013a2143bd5e63c4f41607374401e3f38311f
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-14 19:26:10 +0300

    feat(job vacancy model): set uuid, fillable, and soft delete

--- job-backoffice/app/Models/JobVacancy.php
+++ job-backoffice/app/Models/JobVacancy.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobVacancy extends Model
 {
-    //
+    use HasFactory, HasUuids, SoftDeletes;
+    protected $table = 'job_vacancies';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'type',
+        'salary',
+        'companyId',
+        'categoryId',
+    ];
+
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'deleted_at' => 'datetime',
+        ];
+    }
 }
