a9cef016778fd12bc859286fba2522af6049b4f9
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-16 03:54:34 +0300

    feat(user seeder): seed the root admin

--- job-backoffice/database/seeders/DatabaseSeeder.php
+++ job-backoffice/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Seed the root admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.coom',
+            'password' => Hash::make('123456789'),
+            'role' => 'admin',
+            'email_verified_at' => now()
         ]);
     }
 }
