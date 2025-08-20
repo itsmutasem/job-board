b683562aacc06c0062e16176f7782c06aafa361c
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-20 23:21:15 +0300

    feat(job category.index view): display create job category flash message

--- job-backoffice/resources/views/job-category/index.blade.php
+++ job-backoffice/resources/views/job-category/index.blade.php
@@ -6,6 +6,14 @@
     </x-slot>
 
     <div class="overflow-x-auto p-6">
+{{--        Flash Messages --}}
+        <div class="absolute inset-x-0 bottom-0 z-50">
+            @if(session('create-job-category'))
+                <div x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 3000)" class="bg-green-100 border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                    {{ session('create-job-category') }}
+                </div>
+            @endif
+        </div>
 
 {{--        Job Category Button --}}
         <div class="flex justify-end items-center">
