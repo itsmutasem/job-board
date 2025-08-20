cd3dc3ddf2088d1c9e174a066bc3347bbe63de83
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-20 22:47:03 +0300

    feat(job-category.create view): add create category form

--- job-backoffice/resources/views/job-category/create.blade.php
+++ job-backoffice/resources/views/job-category/create.blade.php
@@ -5,7 +5,25 @@
         </h2>
     </x-slot>
 
-    <div class="overflow-x-auto py-6">
-
+    <div class="overflow-x-auto p-6">
+        <div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
+            <form action="{{ route('job-categories.store') }}" method="POST">
+                @csrf
+                <div class="mb-4">
+                    <label for="name" class="block text-sm font-medium text-gray-700">
+                        Category Name
+                    </label>
+                    <input type="text" name="name" id="name" class="mt-1 block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                </div>
+                    <div class="flex justify-end space-x-4">
+                    <a href="{{ route('job-categories.index') }}" class="inline-flex items-center py-2 bg-white text-gray-500 rounded-md hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">
+                        Cancel
+                    </a>
+                    <button type="submit" class="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">
+                        Add Category
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </x-app-layout>
--- job-backoffice/resources/views/job-category/index.blade.php
+++ job-backoffice/resources/views/job-category/index.blade.php
@@ -9,7 +9,7 @@
 
 {{--        Job Category Button --}}
         <div class="flex justify-end items-center">
-            <a href="{{ route('job-categories.create') }}" class="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">
+            <a href="{{ route('job-categories.create') }}" class="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">
                 Add Job Category
             </a>
         </div>
