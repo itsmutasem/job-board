eda55e7d8dc67371991c552778471dc01496e2c5
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-20 22:26:28 +0300

    feat(job-category.index view): add "add job category" button

--- job-backoffice/resources/views/job-category/create.blade.php
+++ job-backoffice/resources/views/job-category/create.blade.php
@@ -1,1 +1,11 @@
-<?php
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Add Job Category') }}
+        </h2>
+    </x-slot>
+
+    <div class="overflow-x-auto py-6">
+
+    </div>
+</x-app-layout>
--- job-backoffice/resources/views/job-category/index.blade.php
+++ job-backoffice/resources/views/job-category/index.blade.php
@@ -5,7 +5,15 @@
         </h2>
     </x-slot>
 
-    <div class="overflow-x-auto">
+    <div class="overflow-x-auto p-6">
+
+{{--        Job Category Button --}}
+        <div class="flex justify-end items-center">
+            <a href="{{ route('job-categories.create') }}" class="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">
+                Add Job Category
+            </a>
+        </div>
+
 {{--        Job Category Table --}}
         <table class="min-w-full divide-y divide-gray-200 rounded-lg shadow mt-4 bg-white">
             <thead>
@@ -40,3 +48,4 @@
         </div>
     </div>
 </x-app-layout>
+
