7030afb83dd99c2a80a81f7294dec806afdd33d2
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-22 23:33:08 +0300

    !!!temp refactor: using validate in category controller when update rather than validate request!!!

--- job-backoffice/app/Http/Controllers/JobCategoryController.php
+++ job-backoffice/app/Http/Controllers/JobCategoryController.php
@@ -56,10 +56,12 @@ class JobCategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(JobCategoryCreateRequest $request, string $id)
+    public function update(Request $request, string $id)
     {
-        $validate = $request->validated();
         $category = JobCategory::findOrFail($id);
+        $validate = $request->validate([
+            'name' => 'required|string|max:255|unique:job_categories,name,' . $id
+        ]);
         $category->update($validate);
         return redirect()->route('job-categories.index')->with('update', 'Job category updated successfully!');
     }
--- job-backoffice/app/Http/Requests/JobCategoryUpdateRequset.php
+++ job-backoffice/app/Http/Requests/JobCategoryUpdateRequset.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class JobCategoryUpdateRequset extends FormRequest
 {
@@ -22,7 +23,8 @@ class JobCategoryUpdateRequset extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255|unique:job_categories,name,' . $this->category->id,
+        'name' => 'required|string|max:255|unique:job_categories,name,' . $this->$category->id,
+//            'name' => ['required', 'string', 'max:255', Rule::unique('job_categories', 'name')->ignore($this->$category->id)],
         ];
     }
 
