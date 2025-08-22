47e0a101d7529e6a1c6e12e9902eb7f1febb4794
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-22 21:38:02 +0300

    feat(job category.edit view): set a header and layouts

--- job-backoffice/resources/views/job-category/edit.blade.php
+++ job-backoffice/resources/views/job-category/edit.blade.php
@@ -1,1 +1,10 @@
-<?php
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Job Categories') }}
+        </h2>
+    </x-slot>
+
+    
+</x-app-layout>
+
