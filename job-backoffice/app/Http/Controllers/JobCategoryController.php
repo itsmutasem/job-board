53e79c7fe38d46b1dd252d81eb733bf8a2c4830c
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-22 21:58:52 +0300

    feat(job category controller): set the update category functionality

--- job-backoffice/app/Http/Controllers/JobCategoryController.php
+++ job-backoffice/app/Http/Controllers/JobCategoryController.php
@@ -56,9 +56,12 @@ class JobCategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(JobCategoryRequest $request, string $id)
     {
-        //
+        $validate = $request->validated();
+        $category = JobCategory::findOrFail($id);
+        $category->update($validate);
+        return redirect()->route('job-$categories.index')->with('update', 'Job category updated successfully!');
     }
 
     /**
