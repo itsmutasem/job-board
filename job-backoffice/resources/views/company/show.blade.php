005db2b20120844b5a35b32a60841d34bd7fdc15
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-26 23:19:31 +0300

    feat(company.show view): add back button to companies index page
    Introduced a 'Back' button at the top of the company details view to allow users to easily navigate back to the companies index page.

--- job-backoffice/resources/views/company/show.blade.php
+++ job-backoffice/resources/views/company/show.blade.php
@@ -8,6 +8,11 @@
     <div class="overflow-x-auto p-6">
         <x-toast-notification />
 
+{{--        Back Button --}}
+        <div class="mb-6">
+            <a href="{{ route('companies.index') }}" class="bg-gray-200 text-gray-800 hover:bg-gray-300 px-4 py-2 rounded-md shadow">← Back</a>
+        </div>
+
         <div class="w-full mx-auto p-6 bg-white rounded-lg shadow">
             {{-- Company Details --}}
             <div>
