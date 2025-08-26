cb2701007677f6a359fb91c206ee31c50044e3c2
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-26 22:00:31 +0300

    feat(company.show view): add company details view
    Replaces the placeholder with a full company details layout, including name, address, industry, and website. Adds header, notification, and improved styling for better user experience.

--- job-backoffice/resources/views/company/show.blade.php
+++ job-backoffice/resources/views/company/show.blade.php
@@ -1,1 +1,22 @@
-<?php
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $company->name }}
+        </h2>
+    </x-slot>
+
+    <div class="overflow-x-auto p-6">
+        <x-toast-notification />
+
+        <div class="w-full mx-auto p-6 bg-white rounded-lg shadow">
+            {{-- Company Details --}}
+            <div>
+                <h3 class="text-lg font-bold">Company Information</h3>
+                <p><strong>Address:</strong> {{ $company->address }}</p>
+                <p><strong>Industry:</strong> {{ $company->industry }}</p>
+                <p><strong>Website:</strong> <a class="text-blue-500 hover:text-blue-700 underline" href="{{ $company->webiste }}" target="_blank">{{ $company->website }}</a></p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
