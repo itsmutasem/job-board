1deef4f23feff35bf12de61c09a46d0eaf483f96
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-27 15:23:00 +0300

    refactor(company.create): clarify website field as optional in company form
    Updated the label for the website input field in the company creation form to indicate that it is optional, improving form clarity for users.

--- job-backoffice/resources/views/company/create.blade.php
+++ job-backoffice/resources/views/company/create.blade.php
@@ -51,7 +51,7 @@
 
                         <div class="mb-4">
                             <label for="website" class="block text-sm font-medium text-gray-700">
-                                Website
+                                Website (optional)
                             </label>
                             <input type="text" name="website" id="website" value="{{ old('website') }}" class="{{ $errors->has('website') ? 'outline-red-500 outline outline-1' : '' }} mt-1 block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             @error('website')
