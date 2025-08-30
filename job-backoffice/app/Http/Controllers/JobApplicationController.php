dc0f0c547e6e6e91ea0d87f7ecd783f666a6667a
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-30 17:43:21 +0300

    remove(job application controller/create & store)

--- job-backoffice/app/Http/Controllers/JobApplicationController.php
+++ job-backoffice/app/Http/Controllers/JobApplicationController.php
@@ -23,22 +23,6 @@ class JobApplicationController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
