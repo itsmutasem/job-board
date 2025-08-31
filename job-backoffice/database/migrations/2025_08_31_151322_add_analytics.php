cd27fcae48d51ffc1effc1c79c40330e0670b944
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-08-31 18:28:26 +0300

    fix(add_analytics migrations): use timestamp instead of timestamps for last_login_at

--- job-backoffice/database/migrations/2025_08_31_151322_add_analytics.php
+++ job-backoffice/database/migrations/2025_08_31_151322_add_analytics.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamps('last_login_at')->nullable();
+            $table->timestamp('last_login_at')->nullable();
         });
 
         Schema::table('job_vacancies', function (Blueprint $table) {
