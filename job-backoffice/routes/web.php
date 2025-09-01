35401d9fcc5e1a6368d0fcc4a2a6cfdfdfc13ad3
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-09-01 18:16:57 +0300

    feat(web route): set company and admin routes

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -13,25 +13,39 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth', 'role:admin,company-owner'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::resource('companies', CompanyController::class);
-    Route::put('companies/{id}/restore', [CompanyController::class, 'restore'])->name('companies.restore');
 
     Route::resource('job-applications', JobApplicationController::class);
     Route::put('job-applications/{id}/restore', [JobApplicationController::class, 'restore'])->name('job-applications.restore');
 
-    Route::resource('job-categories', JobCategoryController::class);
-    Route::put('job-categories/{id}/restore', [JobCategoryController::class, 'restore'])->name('job-categories.restore');
 
     Route::resource('job-vacancies', JobVacancyController::class);
     Route::put('job-vacancies/{id}/restore', [JobVacancyController::class, 'restore'])->name('job-vacancies.restore');
 
-    Route::resource('users', UserController::class);
-    Route::put('users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
 
-    
+
 });
 
 // Company Routes
+Route::middleware(['auth', 'role:company-owner'])->group(function () {
+    Route::get('/my-company', [CompanyController::class, 'show'])->name('my-company.show');
+    Route::get('/my-company/edit', [CompanyController::class, 'edit'])->name('my-company.edit');
+    Route::put('/my-company', [CompanyController::class, 'update'])->name('my-company.update');
+});
 
 // Admin Routes
+Route::middleware(['auth', 'role:admin'])->group(function (){
+    // Users
+    Route::resource('users', UserController::class);
+    Route::put('users/{id}/restore', [UserController::class, 'restore'])->name('users.restore');
+
+    // Companies
+    Route::resource('companies', CompanyController::class);
+    Route::put('companies/{id}/restore', [CompanyController::class, 'restore'])->name('companies.restore');
+
+    // Job Categories
+    Route::resource('job-categories', JobCategoryController::class);
+    Route::put('job-categories/{id}/restore', [JobCategoryController::class, 'restore'])->name('job-categories.restore');
+
+});
+
 require __DIR__.'/auth.php';
