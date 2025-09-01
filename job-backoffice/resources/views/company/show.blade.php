2bbc9bb8e3c8abb159aa12a901f565e9d5a4fe7d
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-09-01 19:54:45 +0300

    feat(company view/show): edit company buttons for company owner

--- job-backoffice/resources/views/company/show.blade.php
+++ job-backoffice/resources/views/company/show.blade.php
@@ -27,8 +27,14 @@
 
             <div class="flex justify-end space-x-4 mb-6">
                 {{-- Edit Button --}}
-                <a href="{{ route('companies.edit', ['company' => $company->id, 'redirectToList' => 'false']) }}"
-                   class="inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">Edit</a>
+                @if(auth()->user()->role == 'company-owner')
+                    <a href="{{ route('my-company.edit')}}"
+                       class="inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">Edit</a>
+                @else
+                    <a href="{{ route('companies.edit', ['company' => $company->id, 'redirectToList' => 'false']) }}"
+                       class="inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 mt-4 mr-4">Edit</a>
+                @endif
+
                 {{-- Archive Button --}}
                 @if(auth()->user()->role == 'admin')
                 <form action="{{ route('companies.destroy', $company->id) }}" method="POST"
