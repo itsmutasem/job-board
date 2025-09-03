7f09d086a74c04c5c591e32f6031c4082e8a83ea
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-09-03 17:57:31 +0300

    style(dashboard view): search bar and filters

--- job-app/resources/views/dashboard.blade.php
+++ job-app/resources/views/dashboard.blade.php
@@ -1,15 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-white leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="bg-black shadow-lg rounded-lg p-6 max-w-7xl mx-auto">
+            <h3 class="text-white text-2xl font-bold mb-6">
+                {{ __('Welcome back,') }} {{ Auth::user()->name }}!
+            </h3>
+
+{{--            Search & Filters --}}
+            <div class="flex items-center justify-between">
+{{--                Search Bar --}}
+                <form action="" class="flex items-center justify-center w-1/4">
+                    <input type="text" class="w-full rounded-l-lg bg-white/10 text-white border-white/10" placeholder="search for a job">
+                    <button type="submit" class="bg-indigo-500 text-white p-2 rounded-r-lg border border-indigo-500">Search</button>
+                </form>
+
+{{--                Filters --}}
+                <div class="flex space-x-2">
+                    <a href="" class="bg-indigo-500 text-white p-2 rounded-lg">Full Time</a>
+                    <a href="" class="bg-indigo-500 text-white p-2 rounded-lg">Remote</a>
+                    <a href="" class="bg-indigo-500 text-white p-2 rounded-lg">Hybrid</a>
+                    <a href="" class="bg-indigo-500 text-white p-2 rounded-lg">Contract</a>
                 </div>
             </div>
         </div>
